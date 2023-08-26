00edbfcd90e3959dde536e42615f9bcf236c3afc
Marif <syedmarif.professional@gmail.com>
2023-08-26 18:09:42 +0530

    a

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,44 @@
+
+interface Vehicle{
+    void vehicleName();
+    void vehicleNumber();
+}
+
+interface Plane{
+    void planeName();
+}
+
+class Altorz implements Vehicle, Plane{
+    @Override
+    public void vehicleName() {
+        System.out.println("Tata Altroz");
+    }
+
+    @Override
+    public void vehicleNumber() {
+        System.out.println("4468");
+    }
+
+    @Override
+    public void planeName() {
+        System.out.println("Aeroplane");
+    }
+}
+
+class Alto implements Vehicle{
+    @Override
+    public void vehicleName(){
+        System.out.println("Suzuki Alto");
+    }
+
+    @Override
+    public void vehicleNumber() {
+        System.out.println("800");
+    }
+}
+
 public class Car {
+
 
     String getCarName() {
         return "Car";
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 
 class Cal{
     int divide(int a, int b){
@@ -13,17 +13,36 @@ class Cal{
     }
 }
 
+class CarOver extends Car{
+
+    @Override
+    String[] getCarNames(){
+        super.getCarNames();
+        return new String[]{"ca1", "car2"};
+    }
+}
 class Main{
     public static void main (String[] args){
-        Cal cal, cal1, cal2;
+        Cal cal;
         cal = new Cal();
         System.out.println("Salaam");
         System.out.println(cal.divide(10,0));
         System.out.println(cal.divide(11,6));
 
-        System.out.println("getMyCarName(): "+Car.getMyCarName());
-        System.out.println("getCarName(): "+ new Car().getCarName());
+        Car car = new CarOver();
 
+        System.out.println("getMyCarName(): "+Car.getMyCarName());
+        System.out.println("getCarName(): "+ Arrays.toString(new Car().getCarNames()));
+
+        Plane plane = new Altorz();
+        plane.planeName();
+
+        Vehicle vehicle = new Altorz();
+        vehicle.vehicleName();
+
+        Altorz altorz = new Altorz();
+        altorz.planeName();
+        altorz.vehicleName();
 
 
     }
